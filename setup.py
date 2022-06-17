c647abc352169797cc1809d2be12515051e5205c
Bilal Wehbe <Bilal Wehbe>
2022-06-17 13:37:09 +0200

    basic setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+   name='SONAVision',
+   version='0.1.0',
+#   author='An Awesome Coder',
+#   author_email='aac@example.com',
+#   packages=['sonavision'],
+#   scripts=[''],
+#   license='',
+   description='An awesome package that does something',
+   install_requires=["numpy","tensorflow"]
+)
